3f31a6fa8a9c55fd3d24e2a26c6811f9b6cb684e
andychung <2016devx@gmail..com>
2023-05-06 17:38:51 +0800

    Update Demo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="date-list">
+      <div class="date-item" v-for="(date, $index) in dateList" :key="$index">
+        <date-table :date="new Date(date)"></date-table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DateTable from "element-ui/packages/date-picker/src/basic/date-table.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DateTable,
+  },
+
+  data() {
+    return {
+      dateList: [
+        "2023-01-01",
+        "2023-02-01",
+        "2023-03-01",
+        "2023-04-01",
+        "2023-05-01",
+        "2023-06-01",
+        "2023-07-01",
+        "2023-08-01",
+        "2023-09-01",
+        "2023-10-01",
+        "2023-11-01",
+        "2023-12-01",
+      ],
+    };
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +46,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.date-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.date-item {
+  flex: 0 0 25%;
+  padding: 10px;
+  box-sizing: border-box;
+  // background: rgba(153, 228, 253, 0.557);
+  display: flex;
+  justify-content: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+import "element-ui/lib/theme-chalk/index.css";
+
+import "./style.scss";
+
+import ElementUI from "element-ui";
+Vue.use(ElementUI);
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
